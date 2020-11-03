ced5eac25178f14043819ed80ab17ccba5b18028
hvalovyura <yuri.hvalov@it-symphony.com>
2020-11-03 22:18:01 +0300

    Adding interfaces

---
+++ Shop/Shop/Interfaces/IAllProducts.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shop.Interfaces
+{
+    interface IAllProducts
+    {
+        IEnumerable<Product> Products { get; set; }
+        Product GetObjectProduct(int id);
+    }
+}
--- Shop/Shop/Models/Product.cs
+++ Shop/Shop/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +8,10 @@ namespace Shop.Models
 {
     public class Product
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+        public virtual Category Category { get; set; }
     }
 }
