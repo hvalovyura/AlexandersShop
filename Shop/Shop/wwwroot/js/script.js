742379fac10108bf1c6975cbc42394d665a596ee
hvalovyura <yuri.hvalov@it-symphony.com>
2020-11-09 03:03:58 +0300

    Removing commented code

--- Shop/Shop/wwwroot/js/script.js
+++ Shop/Shop/wwwroot/js/script.js
@@ -1,14 +1,6 @@
 ﻿document.getElementById('widget-button').onclick = function () {
     document.getElementById('widget-frame').style = 'display: block';
     document.getElementById('close-button').style = 'display: block';
-    //var closeButton = document.createElement('a');
-    //closeButton.id = 'close-button';
-    //var closeButtonText = document.createElement('i');
-    //closeButtonText.id = 'close-button-text';
-    //closeButtonText.textContent = 'X';
-    //closeButton.appendChild(closeButtonText);
-    //document.body.appendChild(closeButton);
-
     document.getElementById('widget-button').style = 'display: none';
 }
 
