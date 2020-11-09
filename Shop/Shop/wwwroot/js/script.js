5042487c87724e19778bbf39cefaecd74f407333
hvalovyura <yuri.hvalov@it-symphony.com>
2020-11-09 03:02:36 +0300

    Adding widget with eternal source

---
+++ Shop/Shop/wwwroot/js/script.js
@@ -0,0 +1,19 @@
+﻿document.getElementById('widget-button').onclick = function () {
+    document.getElementById('widget-frame').style = 'display: block';
+    document.getElementById('close-button').style = 'display: block';
+    //var closeButton = document.createElement('a');
+    //closeButton.id = 'close-button';
+    //var closeButtonText = document.createElement('i');
+    //closeButtonText.id = 'close-button-text';
+    //closeButtonText.textContent = 'X';
+    //closeButton.appendChild(closeButtonText);
+    //document.body.appendChild(closeButton);
+
+    document.getElementById('widget-button').style = 'display: none';
+}
+
+document.getElementById('close-button').onclick = function () {
+    document.getElementById('widget-frame').style = 'display: none';
+    document.getElementById('close-button').style = 'display: none';
+    document.getElementById('widget-button').style = 'display: block';
+}
